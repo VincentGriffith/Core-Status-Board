fccce77333bcf8e671e48980d875739dbc80f85e
VincentGriffith <vlgriffith@gmail.com>
2017-05-12 18:28:55 -0700

    Add files via upload

--- CSB.py
+++ CSB.py
@@ -1,4 +1,7 @@
-from tkinter import *
+try: # windows requires lowercase, mac uppercase
+    from Tkinter import *
+except:
+    from tkinter import *
 
 #Config stuff
 elementr = 15 #Fuel element display radius in pixels
@@ -35,11 +38,12 @@ elements = [] #should import from file eventually
 selected1 = None
 selected2 = None #Can select up to 2 positions at a time
 selectmult = False #toggles selecting 1 or 2 positions
+mode = None
 gridnameraw = [("B1",0,4.05384),("B2",3.51053,2.02692),("B3",3.51053,-2.02692),("B4",0,-4.05384),("B5",-3.51053,-2.02692),("B6",-3.51053,2.02692),("C1",0,7.98068),("C2",3.99034,6.91134),("C3",6.91134,3.99034),("C4",7.98068,0),("C5",6.91134,-3.99034),("C6",3.99034,-6.91134),("C7",0,-7.98068),("C8",-3.99034,-6.91134),("C9",-6.91134,-3.99034),("C10",-7.98068,0),("C11",-6.9113,3.9903),("C12",-3.9903,6.9113),("D1",0,11.9456),("D2",4.0853,11.2253),("D3",7.6787,9.1504),("D4",10.3449,5.9728),("D5",11.764,2.0737),("D6",11.764,-2.0737),("D7",10.3449,-5.9728),("D8",7.6787,-9.1504),("D9",4.0853,-11.2253),("D10",0,-11.9456),("D11",-4.0853,-11.2253),("D12",-7.6787,-9.1504),("D13",-10.3449,-5.9728),("D14",-11.764,-2.0737),("D15",-11.764,2.0737),("D16",-10.3449,5.9728),("D17",-7.6787,9.1504),("D18",-4.0853,11.2253),("E1",0,15.9156),("E2",4.1189,15.3728),("E3",7.9578,13.7828),("E4",11.254,11.254),("E5",13.8742,7.9578),("E6",15.3728,4.1189),("E7",15.9156,0),("E8",15.3728,-4.1189),("E9",13.8742,-7.9578),("E10",11.254,-11.254),("E11",7.9578,-13.7828),("E12",4.1189,-15.3728),("E13",0,-15.9156),("E14",-4.1189,-15.3728),("E15",-7.9578,-13.7828),("E16",-11.254,-11.254),("E17",-13.8742,-7.9578),("E18",-15.3728,-4.1189),("E19",-15.9156,0),("E20",-15.3728,4.1189),("E21",-13.8742,7.9578),("E22",-11.254,11.254),("E23",-7.9578,13.7828),("E24",-4.1189,15.3728),("F1",0,19.8882),("F2",4.134866,19.45259),("F3",8.08863,18.15786),("F4",11.69035,16.08963),("F5",14.77899,13.307314),("F6",17.223232,9.9441),("F7",18.915634,6.14553),("F8",19.777202,2.070608),("F9",19.777202,-2.070608),("F10",18.915634,-6.14553),("F11",17.223232,-9.9441),("F12",14.77899,-13.307314),("F13",11.69035,-16.08963),("F14",8.08863,-18.167858),("F15",4.134866,-19.45259),("F16",0,-19.8882),("F17",-4.134866,-19.45259),("F18",-8.08863,-18.167858),("F19",-11.69035,-16.08963),("F20",-14.77899,-13.307314),("F21",-17.223232,-9.9441),("F22",-18.915634,-6.14553),("F23",-19.777202,-2.070608),("F24",-19.777202,2.070608),("F25",-18.915634,6.14553),("F26",-17.223232,9.9441),("F27",-14.77899,13.307314),("F28",-11.69035,16.08963),("F29",-8.08863,18.167858),("F30",-4.134866,19.45259)] #Core grid positions in real coordinates
 
 gridnamexy = [(raw[0],(raw[1]+50)*180,(raw[2]+22)*180) for raw in gridnameraw] #Translate into distance in pixels from upper left corner of screen
 
-class Outside: #contains and draws elements not in other positions
+class Outside: #contains and draws elements not in core or racks
     def __init__(self):
         self.outs = []
     def add(self,new):
@@ -76,21 +80,30 @@ class Element:
         out.add(self)
     def select(event):
         if selected1 == self:
-            selected1 = None
-            selected2 = None
+            clear_selections()
+            self.unhighlight()
         elif selected1 == None:
             selected1 = self
+            self.highlight()
+            if not selectmult:
+                use_selection()
         elif selectmult and select2 == None:
             selected2 = self
+            self.highlight()
+            use_selection()
         else:
-            selected1 = None
-            selected2 = None
+            clear_selections()
+            self.unhighlight()
     def insert(self, pos):
         self.position = pos
         pos.insert(self)
     def remove(self):
         pos.remove()
         self.insert(None)
+    def highlight(self):
+        pass #will change later to visually indicate selection
+    def unhighlight(self):
+        pass #will change later to remove highlight
 
 class Position:
     def __init__(self,x,y,r,name):
@@ -98,14 +111,60 @@ class Position:
         self.label = mycanvas.create_text(x,y-5,text=name)
         self.name = "name"
         self.contains = None
+        mycanvas.tag_bind(self.circle,'<Button-1>',self.click)
     def insert(self,element):
         self.contains = element
         mycanvas.itemconfig(self.label,text = name + "\n" + element.serial)
     def remove(self):
         self.insert(None)
+    def click(self,event):
+        if self.contains != None:
+            self.contains.select()
+        else:
+            if selected1 == self:
+                clear_selections()
+                self.unhighlight()
+            elif selected1 == None:
+                selected1 = self
+                self.highlight()
+                if not selectmult:
+                    use_selection()
+            elif selectmult and select2 == None:
+                selected2 = self
+                self.highlight()
+                use_selection()
+            else:
+                clear_selections()
+                self.unhighlight()
 
 def select(event):
     pass
+    #coming back to this code later, I'm honestly not sure what I inteded for this, but I'm leaving it as a possible reminder if I remember my original intention
+
+def clear_selections():
+    selected1 = None
+    selected2 = None
+
+def use_selection():
+    if mode == "move":
+        move(selected1,selected2)
+
+def move(first,second):
+    if type(first) is Position and type(second) is Position:
+        clear_selections() #clear selections, input is nonsense
+        print('your test is working')
+    elif type(first) is Element and type(second) is Position:
+        first.position.contains = None
+        first.position = second
+        second.contains = first #move first into second
+    elif type(first) is Position and type(second) is Element:
+        move(second,first) #selections are backwards
+    else: #two elements selected, switch them
+        first.position.contains = second
+        second.position.contains = first
+        first.position, second.position = second.position, first.position #switch position pointers
+
+
 
 class AddElement:
     def __init__(self, parent):
@@ -135,10 +194,11 @@ class AddElement:
 def add_element():
     add = AddElement(root)
 
-def move_element():
-    pass
-
-def del_element():
+def move_mode():
+    selectmult = True
+    mode = "move"
+ 
+def del_mode():
     pass
 
 def EmptyPoss(poss):
@@ -157,8 +217,8 @@ def makepositions(poslist,r):
 menubar = Menu(root)
 elemenu = Menu(menubar,tearoff = 0)
 elemenu.add_command(label = "Add", command = add_element)
-elemenu.add_command(label = "Move", command = move_element)
-elemenu.add_command(label = "Delete", command = del_element)
+elemenu.add_command(label = "Move", command = move_mode)
+elemenu.add_command(label = "Delete", command = del_mode)
 menubar.add_cascade(label = "Elements", menu = elemenu)
 
 root.config(menu=menubar)
